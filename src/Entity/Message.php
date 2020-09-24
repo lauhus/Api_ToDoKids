4720e3665ae033e0ca6feac93adaf948fb6f7bb9
lauhus <laurine.hussong@hotmail.fr>
2020-09-24 09:35:18 +0200

    Import class Annotation groups

--- src/Entity/Message.php
+++ src/Entity/Message.php
@@ -3,6 +3,7 @@
 namespace App\Entity;
 
 use ApiPlatform\Core\Annotation\ApiResource;
+use Symfony\Component\Serializer\Annotation\Groups;
 use App\Repository\MessageRepository;
 use Doctrine\ORM\Mapping as ORM;
 
